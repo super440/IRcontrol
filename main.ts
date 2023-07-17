9729db9bdd013f4d75a20cb1850b08d7e270e92f
super440 <13189727889@163.com>
2023-07-17 18:05:44 +0800

    IRcontrol
    BuildGenius Robit by Micro:bit

---
+++ main.ts
@@ -0,0 +1,416 @@
+
+const enum IrButton {
+    Up = 2,
+    Down = 152,
+    Left = 224,
+    Right = 144,
+	SET = 168,
+    A = 162,
+    B = 98,
+	C = 226,
+	D = 34,
+	E = 194,
+	F = 176,
+    Num0 = 104,
+    Num1 = 10,
+    Num2 = 24,
+    Num3 = 10,
+    Num4 = 16,
+    Num5 = 56,
+    Num6 = 90,
+    Num7 = 83,
+    Num8 = 74,
+    Num9 = 10,
+	Any = -1
+}
+
+const enum IrButtonAction {
+    //% block="pressed"
+    Pressed = 0,
+    //% block="released"
+    Released = 1,
+}
+
+const enum IrProtocol {
+    //% block="Other"
+    Other = 0,
+    //% block="NEC"
+    NEC = 1,
+}
+
+
+
+
+//% weight=12 color=#ff0000 icon="\uf1eb" block="IRcontrol"
+namespace IRcontrol {
+    let irState: IrState;
+
+    const IR_REPEAT = 256;
+    const IR_INCOMPLETE = 257;
+    const IR_DATAGRAM = 258;
+
+    const REPEAT_TIMEOUT_MS = 120;
+
+    interface IrState {
+        protocol: IrProtocol;
+        hasNewDatagram: boolean;
+        bitsReceived: uint8;
+        addressSectionBits: uint16;
+        commandSectionBits: uint16;
+        hiword: uint16;
+        loword: uint16;
+        activeCommand: number;
+        repeatTimeout: number;
+        onIrButtonPressed: IrButtonHandler[];
+        onIrButtonReleased: IrButtonHandler[];
+        onIrDatagram: () => void;
+    }
+    class IrButtonHandler {
+        irButton: IrButton;
+        onEvent: () => void;
+
+        constructor(
+            irButton: IrButton,
+            onEvent: () => void
+        ) {
+            this.irButton = irButton;
+            this.onEvent = onEvent;
+        }
+    }
+
+
+    function appendBitToDatagram(bit: number): number {
+        irState.bitsReceived += 1;
+
+        if (irState.bitsReceived <= 8) {
+            irState.hiword = (irState.hiword << 1) + bit;
+            if (irState.protocol === IrProtocol.Other && bit === 1) {
+                irState.bitsReceived = 9;
+                irState.hiword = 1;
+            }
+        } else if (irState.bitsReceived <= 16) {
+            irState.hiword = (irState.hiword << 1) + bit;
+        } else if (irState.bitsReceived <= 32) {
+            irState.loword = (irState.loword << 1) + bit;
+        }
+
+        if (irState.bitsReceived === 32) {
+            irState.addressSectionBits = irState.hiword & 0xffff;
+            irState.commandSectionBits = irState.loword & 0xffff;
+            return IR_DATAGRAM;
+        } else {
+            return IR_INCOMPLETE;
+        }
+    }
+
+    function decode(markAndSpace: number): number {
+        if (markAndSpace < 1600) {
+            // low bit
+            return appendBitToDatagram(0);
+        } else if (markAndSpace < 2700) {
+            // high bit
+            return appendBitToDatagram(1);
+        }
+
+        irState.bitsReceived = 0;
+
+        if (markAndSpace < 12500) {
+            // Repeat detected
+            return IR_REPEAT;
+        } else if (markAndSpace < 14500) {
+            // Start detected
+            return IR_INCOMPLETE;
+        } else {
+            return IR_INCOMPLETE;
+        }
+    }
+
+    function enableIrMarkSpaceDetection(pin: DigitalPin) {
+        pins.setPull(pin, PinPullMode.PullNone);
+
+        let mark = 0;
+        let space = 0;
+
+        pins.onPulsed(pin, PulseValue.Low, () => {
+            // HIGH, see https://github.com/microsoft/pxt-microbit/issues/1416
+            mark = pins.pulseDuration();
+        });
+
+        pins.onPulsed(pin, PulseValue.High, () => {
+            // LOW
+            space = pins.pulseDuration();
+            const status = decode(mark + space);
+
+            if (status !== IR_INCOMPLETE) {
+                handleIrEvent(status);
+            }
+        });
+    }
+
+    function handleIrEvent(irEvent: number) {
+        // Refresh repeat timer
+        if (irEvent === IR_DATAGRAM || irEvent === IR_REPEAT) {
+            irState.repeatTimeout = input.runningTime() + REPEAT_TIMEOUT_MS;
+        }
+
+        if (irEvent === IR_DATAGRAM) {
+            irState.hasNewDatagram = true;
+
+            if (irState.onIrDatagram) {
+                background.schedule(irState.onIrDatagram, background.Thread.UserCallback, background.Mode.Once, 0);
+            }
+
+            const newCommand = irState.commandSectionBits >> 8;
+
+            // Process a new command
+            if (newCommand !== irState.activeCommand) {
+
+                if (irState.activeCommand >= 0) {
+                    const releasedHandler = irState.onIrButtonReleased.find(h => h.irButton === irState.activeCommand || IrButton.Any === h.irButton);
+                    if (releasedHandler) {
+                        background.schedule(releasedHandler.onEvent, background.Thread.UserCallback, background.Mode.Once, 0);
+                    }
+                }
+
+                const pressedHandler = irState.onIrButtonPressed.find(h => h.irButton === newCommand || IrButton.Any === h.irButton);
+                if (pressedHandler) {
+                    background.schedule(pressedHandler.onEvent, background.Thread.UserCallback, background.Mode.Once, 0);
+                }
+
+                irState.activeCommand = newCommand;
+            }
+            //basic.showNumber(newCommand);//test
+        }
+    }
+
+    function initIrState() {
+        if (irState) {
+            return;
+        }
+
+        irState = {
+            protocol: undefined,
+            bitsReceived: 0,
+            hasNewDatagram: false,
+            addressSectionBits: 0,
+            commandSectionBits: 0,
+            hiword: 0, // TODO replace with uint32
+            loword: 0,
+            activeCommand: -1,
+            repeatTimeout: 0,
+            onIrButtonPressed: [],
+            onIrButtonReleased: [],
+            onIrDatagram: undefined,
+        };
+    }
+
+    //% blockId="connectIrReceiver"
+    //% block="Connect IR receiver at pin %pin"
+    //% pin.fieldEditor="gridpicker"
+    //% pin.fieldOptions.tooltips="false"
+    //% weight=90
+    export function connectIrReceiver(
+        pin: DigitalPin
+    ): void {
+        initIrState();
+
+        irState.protocol = IrProtocol.NEC;
+        //basic.showNumber(0);
+        enableIrMarkSpaceDetection(pin);
+
+        background.schedule(notifyIrEvents, background.Thread.Priority, background.Mode.Repeat, REPEAT_TIMEOUT_MS);
+    }
+
+    function notifyIrEvents() {
+        if (irState.activeCommand === -1) {
+            // skip to save CPU cylces
+        } else {
+            const now = input.runningTime();
+            if (now > irState.repeatTimeout) {
+                // repeat timed out
+
+                const handler = irState.onIrButtonReleased.find(h => h.irButton === irState.activeCommand || IrButton.Any === h.irButton);
+                if (handler) {
+                    background.schedule(handler.onEvent, background.Thread.UserCallback, background.Mode.Once, 0);
+                }
+
+                irState.bitsReceived = 0;
+                irState.activeCommand = -1;
+            }
+        }
+    }
+
+    //% blockId=onIrButton
+    //% block="On IR button | %button | %action"
+    //% button.fieldEditor="gridpicker"
+    //% button.fieldEditor.width=220
+    //% button.fieldOptions.tooltips="false"
+    //% weight=50
+    export function onIrButton(
+        button: IrButton,
+        action: IrButtonAction,
+        handler: () => void
+    ) {
+        initIrState();
+        if (action === IrButtonAction.Pressed) {
+            irState.onIrButtonPressed.push(new IrButtonHandler(button, handler));
+        }
+        else {
+            irState.onIrButtonReleased.push(new IrButtonHandler(button, handler));
+        }
+    }
+}
+
+namespace IRcontrol {
+    export namespace background {
+
+        export enum Thread {
+            Priority = 0,
+            UserCallback = 1,
+        }
+
+        export enum Mode {
+            Repeat,
+            Once,
+        }
+
+        class Executor {
+            _newJobs: Job[] = undefined;
+            _jobsToRemove: number[] = undefined;
+            _pause: number = 100;
+            _type: Thread;
+
+            constructor(type: Thread) {
+                this._type = type;
+                this._newJobs = [];
+                this._jobsToRemove = [];
+                control.runInParallel(() => this.loop());
+            }
+
+            push(task: () => void, delay: number, mode: Mode): number {
+                if (delay > 0 && delay < this._pause && mode === Mode.Repeat) {
+                    this._pause = Math.floor(delay);
+                }
+                const job = new Job(task, delay, mode);
+                this._newJobs.push(job);
+                return job.id;
+            }
+
+            cancel(jobId: number) {
+                this._jobsToRemove.push(jobId);
+            }
+
+            loop(): void {
+                const _jobs: Job[] = [];
+
+                let previous = control.millis();
+
+                while (true) {
+                    const now = control.millis();
+                    const delta = now - previous;
+                    previous = now;
+
+                    // Add new jobs
+                    this._newJobs.forEach(function (job: Job, index: number) {
+                        _jobs.push(job);
+                    });
+                    this._newJobs = [];
+
+                    // Cancel jobs
+                    this._jobsToRemove.forEach(function (jobId: number, index: number) {
+                        for (let i = _jobs.length - 1; i >= 0; i--) {
+                            const job = _jobs[i];
+                            if (job.id == jobId) {
+                                _jobs.removeAt(i);
+                                break;
+                            }
+                        }
+                    });
+                    this._jobsToRemove = []
+
+
+                    // Execute all jobs
+                    if (this._type === Thread.Priority) {
+                        // newest first
+                        for (let i = _jobs.length - 1; i >= 0; i--) {
+                            if (_jobs[i].run(delta)) {
+                                this._jobsToRemove.push(_jobs[i].id)
+                            }
+                        }
+                    } else {
+                        // Execute in order of schedule
+                        for (let i = 0; i < _jobs.length; i++) {
+                            if (_jobs[i].run(delta)) {
+                                this._jobsToRemove.push(_jobs[i].id)
+                            }
+                        }
+                    }
+
+                    basic.pause(this._pause);
+                }
+            }
+        }
+
+        class Job {
+            id: number;
+            func: () => void;
+            delay: number;
+            remaining: number;
+            mode: Mode;
+
+            constructor(func: () => void, delay: number, mode: Mode) {
+                this.id = randint(0, 2147483647)
+                this.func = func;
+                this.delay = delay;
+                this.remaining = delay;
+                this.mode = mode;
+            }
+
+            run(delta: number): boolean {
+                if (delta <= 0) {
+                    return false;
+                }
+
+                this.remaining -= delta;
+                if (this.remaining > 0) {
+                    return false;
+                }
+
+                switch (this.mode) {
+                    case Mode.Once:
+                        this.func();
+                        basic.pause(0);
+                        return true;
+                    case Mode.Repeat:
+                        this.func();
+                        this.remaining = this.delay;
+                        basic.pause(0);
+                        return false;
+                }
+            }
+        }
+
+        const queues: Executor[] = [];
+
+        export function schedule(
+            func: () => void,
+            type: Thread,
+            mode: Mode,
+            delay: number,
+        ): number {
+            if (!func || delay < 0) return 0;
+
+            if (!queues[type]) {
+                queues[type] = new Executor(type);
+            }
+
+            return queues[type].push(func, delay, mode);
+        }
+
+        export function remove(type: Thread, jobId: number): void {
+            if (queues[type]) {
+                queues[type].cancel(jobId);
+            }
+        }
+    }
+}
